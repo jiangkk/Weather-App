df1e27e685f2df7a9ab87b5323e189d4356d2289
jiangkk <jiangkk@guahao.com>
2022-05-23 22:18:20 +0800

    docs: test pages

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "docs"),
   },
+  base: '/Weather-App/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "src"),
