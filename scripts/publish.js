1e72ba3f7b71d9cab28580ea12bd325bcd8b6b37
jiangkk <jiangkk@guahao.com>
2022-05-24 01:21:38 +0800

    feat: add scripts

--- scripts/publish.js
+++ scripts/publish.js
@@ -2,12 +2,11 @@ const { execSync } = require('child_process');
 
 const exec = cmd => execSync(cmd, { stdio: 'inherit' });
 
-// exec('git checkout pages');
-// exec('git branch --set-upstream-to=origin/pages pages');
-// exec('git pull');
-// exec('git merge main --no-edit');
-// exec('npm i && npm run build');
-// exec('git add . ')
+exec('git checkout pages');
+exec('git branch --set-upstream-to=origin/pages pages');
+exec('git pull');
+exec('git merge main --no-edit');
+exec('npm i && npm run build');
 exec(`git commit -am 'docs: update pages'`);
-// exec('git push origin pages');
-// exec('git checkout -');
+exec('git push origin pages');
+exec('git checkout -');
